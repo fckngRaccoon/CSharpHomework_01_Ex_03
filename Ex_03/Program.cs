9583ab8de92e192a2bf6292c716db0c219dca68f
cppdev <fckngRaccoon@yandex.ru>
2023-03-02 01:21:01 +0300

    Ex_03

---
+++ Ex_03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Ведите число: ");
+int number = Console.Read();
+
+if (number == 0)
+{
+    Console.Write ("Ноль");
+}
+else if (number % 2 == 0)
+{
+    Console.Write ("Четное");
+}
+else
+{
+    Console.Write ("Нечетное");
+}
